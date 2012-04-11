4bfccc5aa094a18bab4d25bd56fd6a201b76665e
Niels Avonds <niels.avonds@gmail.com>
2012-04-11 23:32:43 +0200

    Finished member details section

--- src/delta/site/client/MemberDetailPage.java
+++ src/delta/site/client/MemberDetailPage.java
@@ -1,46 +1,66 @@
 package delta.site.client;
 
-import com.google.gwt.user.client.ui.AbsolutePanel;
+import com.google.gwt.i18n.client.HasDirection.Direction;
 import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.Grid;
 import com.google.gwt.user.client.ui.HTML;
-import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.HasHorizontalAlignment.HorizontalAlignmentConstant;
+import com.google.gwt.user.client.ui.HorizontalPanel;
+import com.google.gwt.user.client.ui.Image;
+import com.google.gwt.user.client.ui.VerticalPanel;
+import com.google.gwt.user.client.ui.Widget;
 
 public class MemberDetailPage extends Composite {
 
 	public MemberDetailPage() {
-		this("Niels Avonds", "Tow be, or not tow be, is that really THE questaajon?", 4, "CW", "IT");
+		this(
+				"Niels Avonds",
+				"Tow be, or not tow be, is that really THE questaajon? This answer is a little longer to test the wrapping over multiple lines.",
+				4, "CW", "IT");
 	}
-	
-	public MemberDetailPage(String name, String quote, int year, String study, String vtkpost) {
-		
-		Grid mainpanel = new Grid(1, 2);
 
-		Grid leftpanel = createLeftPanel(name, quote);
-		Grid rightpanel = createRightPanel();		
+	public MemberDetailPage(String name, String quote, int year, String study,
+			String vtkpost) {
 
-		mainpanel.setWidget(0, 0, leftpanel);
-		mainpanel.setWidget(0, 1, rightpanel);
+		HorizontalPanel mainpanel = new HorizontalPanel();
+		mainpanel.setSize("660px", "260px");
+
+		Widget leftpanel = createLeftPanel(name, quote);
+		Widget rightpanel = createRightPanel(year, study, vtkpost);
+
+		mainpanel.add(leftpanel);
+		mainpanel.add(rightpanel);
 		initWidget(mainpanel);
 	}
-	
-	private Grid createLeftPanel(String name, String quote) {
-		Grid leftpanel = new Grid(2, 1);
+
+	private Widget createLeftPanel(String name, String quote) {
+		VerticalPanel leftpanel = new VerticalPanel();
 
 		HTML html = new HTML("<span class=\"membername\">" + name + "</span>");
-		leftpanel.setWidget(0, 0, html);
-		
-		html = new HTML("<br/><span class=\"memberquestions\">" + 
-							"<b>Favoriete quote:</b><br/>" + quote + "<br/>" +
-							"<b>Nog een vraag:</b><br/>" + "Nog een antwoord" + "</br>" + 
-						"</span>");
-		leftpanel.setWidget(1, 0, html);
-		
+		leftpanel.add(html);
+
+		html = new HTML("<br/><span class=\"memberquestions\">"
+				+ "<b>Favoriete quote:</b><br/>" + quote + "<br/>"
+				+ "<b>Nog een vraag:</b><br/>" + "Nog een antwoord" + "</br>"
+				+ "</span>");
+		leftpanel.add(html);
+		leftpanel.setStylePrimaryName("memberleft");
+
 		return leftpanel;
 	}
-	
-	private Grid createRightPanel() {
-		Grid rightpanel = new Grid(2, 1);
+
+	private Widget createRightPanel(int year, String study, String vtkpost) {
+		VerticalPanel rightpanel = new VerticalPanel();
+
+		Image image = new Image(
+				"http://www.pmradio.com.au/images/mainsite/profile_m.gif");
+		image.setSize("150px", "150px");
+		rightpanel.add(image);
+
+		HTML html = new HTML("<span class=\"memberinfo\">" + "Jaar: " + year
+				+ "e<br/>" + "Richting: " + study + "<br/>"
+				+ "Favoriete post: " + vtkpost + "</span>");
+		rightpanel.add(html);
 
 		return rightpanel;
 	}
